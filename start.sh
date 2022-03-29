1d67e38c862bbd906cc3a827ab7f7d31548f8a22
Mike Purvis <mikedotexe@gmail.com>
2022-03-29 11:06:39 -0700

    tack 9 baby

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps -ef | grep "python launcher.py" | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep "python launcher.py" | grep -v grep | awk '{print $2}' | xargs kill -9 
 node index.js &
 python ipc.py &
 python launcher.py
